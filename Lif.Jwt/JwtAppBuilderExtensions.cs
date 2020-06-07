e9ed934560e9795a0eb0bd299a61fa905cd7ec59
leo <xz@lgysh.cn>
2020-06-07 14:37:14 +0800

    计划封装jwt认证

---
+++ Lif.Jwt/JwtAppBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Lif.Jwt
+{
+    public static class JwtAppBuilderExtensions
+    {
+        public static IApplicationBuilder UseLifJwt(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
---
+++ Lif.Jwt/JwtServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Lif.Jwt
+{
+    public static class JwtServiceCollectionExtensions
+    {
+        public static IServiceCollection AddLifJwt(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
